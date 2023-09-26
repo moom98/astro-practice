a7ebe0fd3be9f15b72e9b171acfcd141fd96e3ac
ymkmy98 <ymkmy98@gmail.com>
2023-09-26 17:04:34 +0900

    ビルドされたhtmlの画像参照先を変更

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,11 @@ import relativeLinks from "astro-relative-links";
 // https://astro.build/config
 export default defineConfig({
   site: "https://ymkmy98.github.io",
-  base: "/astro_practice",
+  base: "",
   outDir: "./dist",
   build: {
     format: "file",
+    assets: "assets/images",
   },
   server: {
     open: true,
@@ -25,20 +26,24 @@ export default defineConfig({
   ],
   vite: {
     build: {
+      assetsInlineLimit: 0,
       rollupOptions: {
         output: {
-          assetFileNames: (assetInfo) => {
-            let extType = assetInfo.name.split(".")[1];
-            if (/ttf|otf|eot|woff|woff2/i.test(extType)) {
-              extType = "fonts";
-            }
+          leNames: (assetInfo) => {
+            let extType = assetInfo.name.split(".").at(-1);
             if (/png|jpe?g|svg|gif|webp|tiff|bmp|ico/i.test(extType)) {
               extType = "images";
             }
-            if (extType === "css") {
-              return `assets/css/style.css`;
+            if (/ts|js/i.test(extType)) {
+              extType = "script";
             }
-            return `assets/${extType}/[name][extname]`;
+            if (/css|scss/i.test(extType)) {
+              extType = "css";
+            }
+            //   if (/ttf|otf|eot|woff|woff2/i.test(extType)) {
+            //     extType = "fonts";
+            //   }
+            return `assets/${extType}/[name]-[hash][extname]`;
           },
         },
         entryFileNames: "assets/js/[name].js",
